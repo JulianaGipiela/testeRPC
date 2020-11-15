9c76945280f4e92cce8502d7cf0994450672d5e8
Juliana Cecília Gipiela Corrêa Dias <jugipiela@gmail.com>
2020-11-15 11:57:21 -0300

    alterações no js

--- resource/js/main.js
+++ resource/js/main.js
@@ -12,36 +12,3 @@ for (i = 0; i < coll.length; i++) {
     } 
   });
 }
-
-var arrayDia = new Array(7);
-arrayDia[0] = "DOMINO";
-arrayDia[1] = "SEGUNDA";
-arrayDia[2] = "TERÇA";
-arrayDia[3] = "QUARTA";
-arrayDia[4] = "QUINTA";
-arrayDia[5] = "SEXTA";
-arrayDia[6] = "SÁBADO";
-
-function getDateWrite(dia){
-    return this.arrayDia[dia];
-}
-
-function timeNow() {
-    var time = new Date();
-    return time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds();
-}
-
-function dateNow() {
-    var now = new Date();
-    var date = getDateWrite(now.getDay());
-
-    return date + " - " + now.getDate() + "/" + now.getMonth() + "/" + now.getFullYear();
-}
-
-
-function getDate() {   
-    document.getElementById("date").innerHTML =  dateNow();
-}
-
-
-getDate();
